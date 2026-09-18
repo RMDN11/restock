64d1d0cf0048469730e5209b888fa1cad060e10e
RMDN11 <hanrmdn11@gmail.com>
2026-09-19 06:20:36 +0700

    fix: correct developer auth include path

--- developer/subscriptions/index.php
+++ developer/subscriptions/index.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 
-require_once __DIR__ . '/../includes/developer_auth.php';
+require_once __DIR__ . '/../../includes/developer_auth.php';
 
 $pageTitle = 'Subscription';
 
