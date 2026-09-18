d67d51a0361589513209c26933ca6907108c25ce
RMDN11 <hanrmdn11@gmail.com>
2026-09-19 06:28:23 +0700

    test: enforce package-first registration flow

--- tests/test_registration_flow.php
+++ tests/test_registration_flow.php
@@ -1,11 +1,14 @@
 <?php
 $login = file_get_contents(__DIR__ . '/../login.php');
 $flow = file_get_contents(__DIR__ . '/../daftar-pilih.php');
+$register = file_get_contents(__DIR__ . '/../daftar.php');
 
 assert(strpos($login, 'href="/daftar-pilih.php"') !== false);
-assert(strpos($login, 'href="/daftar.php"') === false);
 assert(strpos($flow, 'href="/daftar.php"') !== false);
-assert(strpos($flow, 'action="/free-plan.php"') !== false);
-assert(strpos($flow, 'name="token"') !== false);
+assert(strpos($flow, 'Punya undangan Free Plan?') !== false);
+assert(strpos($register, 'Pilih Paket') !== false);
+assert(strpos($register, 'registrationPackages') !== false);
+assert(strpos($register, 'package_id=<?= (int) $package[\'id\'] ?>') !== false);
+assert(strpos($register, "Pilih paket terlebih dahulu sebelum membuat akun.") === false);
 
-echo "Registration flow contract passed.\n";
+echo "Registration package-first flow contract passed.\n";
