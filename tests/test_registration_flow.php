38440bfcac2072f38a233eb2b72a4abe7004cef6
RMDN11 <hanrmdn11@gmail.com>
2026-09-19 21:26:08 +0700

    test: align package flow contracts with canonical route

--- tests/test_registration_flow.php
+++ tests/test_registration_flow.php
@@ -1,9 +1,10 @@
 <?php
 $register = file_get_contents(__DIR__ . '/../daftar.php');
 
-assert(strpos($register, 'Pilih Paket') !== false);
-assert(strpos($register, 'registrationPackages') !== false);
-assert(strpos($register, 'package_id=<?= (int) $package[\'id\'] ?>') !== false);
-assert(strpos($register, "Pilih paket terlebih dahulu sebelum membuat akun.") === false);
+assert(strpos($register, "if (\$_SERVER['REQUEST_METHOD'] === 'GET' && \$packageId === null && \$freePlanToken === '' && !\$freeRegistration)") !== false);
+assert(strpos($register, "header('Location: /daftar-paket.php');") !== false);
+assert(strpos($register, "\$_SESSION['selected_package_id'] = \$packageId;") !== false);
+assert(strpos($register, "\$_SESSION['checkout_origin'] = '/daftar-paket.php';") !== false);
+assert(strpos($register, "header('Location: /checkout.php');") !== false);
 
 echo "Registration package-first flow contract passed.\n";
